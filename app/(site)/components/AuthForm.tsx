b999b5d60e8068b182bf6b260159502c86e7196e
Pranjil97 <pranjilkumar97@gmail.com>
2023-06-02 13:38:35 +0530

    update the signin form install tailwindform packages

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,8 +1,81 @@
 'use client';
 
+import Input from "@/app/components/inputs/input";
+import { useState, useCallback } from "react";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+
+type Variant = 'LOGIN' | 'REGISTER';
+
 const AuthForm = () => {
-    return (
-        <div>AuthForm 2 !</div>
+    const [variant, setVariant] = useState<Variant>('LOGIN');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const toggleVariant = useCallback(() => {
+        if (variant === 'LOGIN') {
+            setVariant('REGISTER');
+        } else {
+            setVariant('LOGIN');
+        }
+    }, [variant]);
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: '',
+
+        }
+    })
+
+    const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+        setIsLoading(true);
+
+        if (variant === 'REGISTER') {
+            // axios register            
+        }
+        if (variant === 'LOGIN') {
+            // nextAuth SignIn
+        }
+
+    };
+
+    const socialAction = (action: string) => {
+        setIsLoading(true);
+        // nextAuth social signIn
+    };
+
+    ; return (
+        <div
+            className="
+                mt-8
+                sm:mx-auto
+                sm:w-full
+                sm:max-w-md
+
+            "
+        >
+            <div
+                className="
+                    bg-white
+                    px-4
+                    py-8
+                    shadow
+                    sm:rounded-lg
+                    sm:px-10
+                "
+            >
+                <form
+                    className="space-y-6"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+                    <Input label="Email" />
+                </form>
+            </div>
+        </div>
     )
 }
 
