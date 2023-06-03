b9d61e5bd5037633eafc70289ac651d6747c5227
Pranjil97 <pranjilkumar97@gmail.com>
2023-06-03 13:01:19 +0530

    set github and google icons button

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -4,6 +4,8 @@ import Button from "@/app/components/Button";
 import Input from "@/app/components/inputs/input";
 import { useState, useCallback } from "react";
 import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub, BsGoogle } from "react-icons/bs";
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -90,6 +92,42 @@ const AuthForm = () => {
                         </Button>
                     </div>
                 </form>
+                <div className="mt-6 ">
+                    <div className="relative">
+                        <div className="
+                            absolute
+                            inset-0
+                            flex
+                            items-center
+                        ">
+                            <div className="
+                                w-full 
+                                border-t
+                                border-gray-300"
+                            />
+                        </div>
+                        <div className="
+                            relative 
+                            flex 
+                            justify-center 
+                            text-sm
+                        ">
+                            <span className="
+                            bg-white 
+                            px-2 
+                            text-gray-500
+                        ">
+                                on continue with
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="mt-6 flex gap-2">
+                        <AuthSocialButton icon={BsGithub} onClick={() => socialAction('github')} />
+                        <AuthSocialButton icon={BsGoogle} onClick={() => socialAction('google')} />
+                    </div>
+
+                </div>
             </div>
         </div>
     )
