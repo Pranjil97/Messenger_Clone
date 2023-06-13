1c837060cdb2e4cc25f6876df6f87f4858cbcb18
Pranjil97 <pranjilkumar97@gmail.com>
2023-06-13 15:22:25 +0530

    create DesktopSidebarProps

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,12 +1,18 @@
 import DesktopSidebar from "./DesktopSidebar";
 import MobileFooter from "./MobileFooter";
 
+import getCurrentUser from "@/app/actions/getCurrentUsers";
+
 async function Sidebar({ children }: {
     children: React.ReactNode
 }) {
+
+    const currentUser = await getCurrentUser();
+    // console.log(currentUser);
+
     return (
         <div className="h-full ">
-            <DesktopSidebar />
+            <DesktopSidebar currentUser={currentUser!} />
             <MobileFooter />
             <main className="lg:pl-20 h-full">
                 {children}
